ceeaf95dc43e328c5675dd37b05a954df2f16655
AllTwinkleStars <KeepGrowinga@gmail.com>
2022-12-16 19:06:08 -0500

    Move audio player to bottom of screen

--- client/src/pages/AudioPlayer.js
+++ client/src/pages/AudioPlayer.js
@@ -3,8 +3,7 @@ import React from "react";
 
 export default ({selectedPodcast}) => {
   const style = {
-      width:"30vw",
-      marginLeft: 'auto',
+      width:"65vw",
   };
   return (
         <ReactAudioPlayer
