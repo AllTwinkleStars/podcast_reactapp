c92c29e5bd825a4945484913e76021a3a9860288
AllTwinkleStars <KeepGrowinga@gmail.com>
2022-12-16 19:09:10 -0500

    Add pls sign in msg to subscription page

--- client/src/pages/SubscriptionsPage.js
+++ client/src/pages/SubscriptionsPage.js
@@ -1,13 +1,33 @@
-import React from "react";
-import { Grid } from "@material-ui/core";
-import PodcastList from './components/PodcastList';
+import React, { useState, useEffect } from "react";
+import { Grid, Typography } from "@material-ui/core";
+import PodcastList from "./components/PodcastList";
 
-
-export default ({subscriptions, setSelectedPodcast}) => {
-
-  return (
-      <Grid item xs={10} style={{ marginTop: "15px" }}>
-        <PodcastList subscriptionList={true} podcasts={subscriptions} onPodcastSelect={setSelectedPodcast}/>
+const SubscriptionsPage = ({
+  subscriptions,
+  setSelectedPodcast,
+  isSignedIn,
+}) => {
+  console.log(subscriptions);
+  return isSignedIn ? (
+    <Grid item xs={10} style={{ marginTop: "15px" }}>
+      <PodcastList
+        isSubscriptionList={true}
+        podcasts={subscriptions}
+        onPodcastSelect={setSelectedPodcast}
+      />
+    </Grid>
+  ) : (
+    <Grid item justify="center" container>
+      <Grid item>
+        <Typography
+          variant="h5"
+          style={{ marginTop: "80px", justifyContent: "center" }}
+        >
+          Please sign in to see your subscriptions
+        </Typography>
       </Grid>
+    </Grid>
   );
-}
+};
+
+export default SubscriptionsPage;
